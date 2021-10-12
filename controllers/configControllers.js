944aff9a135e2082533074356417e14c9c00489b
siskiuslide <jakewhatling2002@gmail.com>
2021-10-12 21:48:33 +0100

    PC Box tweaks + Creating/Deleting configs

--- controllers/configControllers.js
+++ controllers/configControllers.js
@@ -1,60 +1,60 @@
 const Config = require("./../models/configModels");
 
-exports.getConfig = async function(req,res){
-    try{
-      const configs = await Config.find() 
-      res.status(200).json({status: "success", body: configs})
-    }catch(err){
-      res.status(400).json({status: "failed", message: err})    }
-  }
-exports.getSingleConfig = async function(req,res){
-  try{
-    await Config.find({_id: req.params.id}).then(data=>{
-      res.status(200).json({status: "success", data:data})
-
-    })
-  }catch(err){
-    console.log(err)
-    res.status(404).json({status: "failed", message: err})
-  }
-}
-
-exports.createConfig= async function (req, res) {
+exports.getConfig = async function (req, res) {
   try {
-    Config.exists({name: req.body.name}, function(err, result){
-      if (result == true){
-        return res.status(400).json({status: "failed", message: "bad request"})
-      }if(err){console.log(err)}
-    })
-    await Config.create(req.body).then((data) => {
-      res.status(200).json({status: "success", data: req.body });
+    const configs = await Config.find();
+    res.status(200).json({ status: "success", body: configs });
+  } catch (err) {
+    res.status(400).json({ status: "failed", message: err });
+  }
+};
+exports.getSingleConfig = async function (req, res) {
+  try {
+    await Config.find({ _id: req.params.id }).then((data) => {
+      res.status(200).json({ status: "success", data: data });
     });
   } catch (err) {
     console.log(err);
-    res.status(404).json({status: "failed", message: err})
+    res.status(404).json({ status: "failed", message: err });
   }
-}
-exports.updateConfig = async function (object) {
-  try {
-    await Config.findOneAndUpdate({ name: object.name }, object)
-    res.status(200).json({status: "success", data: object})
+};
 
+exports.createConfig = async function (req, res) {
+  try {
+    Config.exists({ name: req.body.name }, function (err, result) {
+      if (result == true) {
+        return res.status(400).json({ status: "failed", message: "bad request" });
+      }
+      if (err) {
+        console.log(err);
+      }
+    });
+    await Config.create(req.body).then((data) => {
+      res.status(200).json({ status: "success", data: req.body });
+    });
   } catch (err) {
     console.log(err);
-    res.status(404).json({status: "Failure", message: err})
+    res.status(404).json({ status: "failed", message: err });
+  }
+};
+exports.updateConfig = async function (object) {
+  try {
+    await Config.findOneAndUpdate({ name: object.name }, object);
+    res.status(200).json({ status: "success", data: object });
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ status: "Failure", message: err });
   }
 };
 
 exports.deleteConfig = async function (req, res) {
-  try{   
-    console.log(req.body)
-      await Config.deleteOne({name:"Test"}).then(response=>{
-        return res.status(200).json({status: "Success", message: {text: "Deleted config from database"}})
-      })
-      
-    }
-  catch(err){
-    console.log(err)
-    return res.status(404).json({status: "failed", message: err})
+  try {
+    console.log(req.body);
+    await Config.deleteOne({ _id: req.body.id }).then((response) => {
+      return res.status(200).json({ status: "Success", message: { text: "Deleted config from database" } });
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ status: "failed", message: err });
   }
-}
+};
--- public/config.js
+++ public/config.js
@@ -3,28 +3,29 @@ const configInputSection = document.querySelector(".configInputSection");
 
 createNewConfig.addEventListener("click", () => {
   fadeOut(createNewConfig, 160);
-  setTimeout(() => {   
+  setTimeout(() => {
     fadeIn(configInputSection, 220, "flex");
   }, 190);
 });
 
-const deleteConfigBtn = document.querySelector('#deleteConfig')
+const deleteConfigBtn = document.querySelector(".preConfigured-delete");
 
-deleteConfigBtn.addEventListener('click', ()=>{
-  const mongoID = deleteConfigBtn.dataset.mongo;
-  const body = {id: mongoID}
-  console.log(mongoID)
-  
-  fetch(`/config`,{
+deleteConfigBtn.addEventListener("click", (e) => {
+  const targetConfig = e.target.closest(".preConfiguredCard");
+  const reqBody = { id: targetConfig.id };
+
+  fetch(`/config`, {
     method: "delete",
-    body: '61657200f9df2a1de86d5f8b',
+    body: JSON.stringify(reqBody),
+    headers: { "content-type": "application/json" },
   })
-  .then(response=>{
-    console.log(response)
-  }).catch(err=>{
-    console.log(err)
-  }
-)})
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 const configToggle = document.querySelector(".configToggle");
 let inputTargetHeader = document.querySelector(".headerTarget");
@@ -43,24 +44,34 @@ const form = document.querySelector(".configForm");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(this);
-  if(!configToggle.checked){
-    formData.type = 'off'
+  if (!configToggle.checked) {
+    formData.type = "off";
   }
   console.log(formData);
   fetch(`127.0.0.1:5500/config`, {
     method: "post",
     body: formData,
-  }).then((res) => console.log(res)).catch(err=>{console.log(err)});
+  })
+    .then((res) => {
+      console.log("x");
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 configSubmitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   form.submit();
 });
-const cancelForm = document.querySelector('.cancelBtn')
-cancelForm.addEventListener('click', ()=>{
-  fadeOut(configInputSection, 160)
-  setTimeout(()=>{
-    fadeIn(createNewConfig, 220, 'flex')
-  }, 190)
-})
+const cancelForm = document.querySelector(".cancelBtn");
+cancelForm.addEventListener("click", () => {
+  fadeOut(configInputSection, 160);
+  setTimeout(() => {
+    fadeIn(createNewConfig, 220, "flex");
+  }, 190);
+});
