6d4306f5d460519260ab946a8a0558092de9a4d3
siskiuslide <jakewhatling2002@gmail.com>
2021-10-13 09:13:14 +0100

    Config page - empty page text

--- public/config.js
+++ public/config.js
@@ -3,6 +3,109 @@ const emptyText = `<div class="emptyText">None to show :/</div>`;
 const createNewConfig = document.querySelector(".createNewConfigBox");
 const configInputSection = document.querySelector(".configInputSection");
 
+const preConfiguredCardSection = document.querySelector(".preConfiguredSection");
+const addCard = function (config) {
+  let configType;
+  config.type == "on" ? (configType = "Cloud") : (configType = "Local");
+  const configCard = `      <div class="preConfiguredCard" id="${config._id}">
+  <div class="preConfiguredHeaderBox">
+      <div class="preConfiguredTitle">${config.name}</div>
+      <div class="preConfiguredIconsSection">
+      <div class="preConfiguredIcon preConfigured-settings">
+        <span class="material-icons refresh-icon" id="preConfiguredIcon">refresh</span>
+      </div>
+      <div class="preConfiguredIcon preConfigured-edit">
+        <span class="material-icons edit-icon" id="preConfiguredIcon">edit</span>
+      </div>
+      <div class="preConfiguredIcon preConfigured-settings">
+        <span class="material-icons settings-icon" id="preConfiguredIcon">settings</span>
+      </div>
+      <div class="preConfiguredIcon preConfigured-delete">
+        <span class="material-icons delete-icon" id="preConfiguredIcon">delete</span>
+      </div>
+    </div>
+  </div>
+  <div class="preConfiguredBody">
+    <div class="split-section pcDetails">
+      <div class="detail-flex">
+        <div class="detailIcon">
+          <span class="material-icons cloudIcon">cloud</span>
+        </div>
+        <div class="detailField">Type</div>
+        <div class="detailText">${configType}</div>              
+      </div>
+      <div class="detail-flex">
+        <div class="detailIcon">
+          <span class="material-icons locationIcon">fmd_good</span>
+        </div>
+        <div class="detailField">Address</div>
+        <div class="detailText detailText-small">${config.target}</div>              
+      </div>
+      <div class="detail-flex">
+        <div class="detailIcon">
+          <span class="material-icons lockIcon">lock</span>
+        </div>
+        <div class="detailField">API Key</div>
+        <div class="detailText detailText-small">${config.APIKey}</div>              
+      </div>
+      <div class="detail-flex">
+        <div class="detailIcon">
+          <span class="material-icons tagIcon">tag</span>
+        </div>
+        <div class="detailField">App ID</div>
+        <div class="detailText">${config.appID}</div>              
+      </div>
+    </div>
+    <div class="split-section pc-right-split">
+      <div class="right-split-item deviceCountContainer">
+        <div class="deviceCount-item deviceCountNumber">COUNT</div>
+        <div class="deviceCount-item deviceCountText">Results</div>
+      </div>   
+    </div>          
+  </div>
+</div>`;
+  preConfiguredCardSection.insertAdjacentHTML("beforeEnd", configCard);
+  setTimeout(() => {
+    document.querySelector(".preConfiguredCard").style.opacity = "1";
+  }, 100);
+};
+//DISPLAY ON LOAD
+window.addEventListener("load", async () => {
+  const rawConfigData = await fetch("/config").then((res) => res.json());
+  const configData = rawConfigData.body;
+
+  if (configData.length == 0) {
+    return preConfiguredCardSection.insertAdjacentHTML("beforeend", emptyText);
+  }
+  configData.forEach((config) => {
+    addCard(config);
+  });
+});
+
+//DELETE
+preConfiguredCardSection.addEventListener("click", async (e) => {
+  if (e.target.classList.contains("delete-icon")) {
+    const targetConfig = e.target.closest(".preConfiguredCard");
+    const reqBody = { id: targetConfig.id };
+
+    await fetch(`/config`, {
+      method: "delete",
+      body: JSON.stringify(reqBody),
+      headers: { "content-type": "application/json" },
+    })
+      .then((response) => {
+        fadeOut(targetConfig, 180);
+        preConfiguredCardSection.removeChild(document.getElementById(`${targetConfig.id}`));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    if (document.querySelector(".preConfiguredCard") == null) {
+      preConfiguredCardSection.insertAdjacentHTML("beforeend", emptyText);
+    }
+  }
+});
+
 createNewConfig.addEventListener("click", () => {
   fadeOut(createNewConfig, 160);
   setTimeout(() => {
@@ -27,6 +130,7 @@ const form = document.querySelector(".configForm");
 configSubmitBtn.addEventListener("click", (e) => {
   form.submit();
   form.reset();
+  window.location.reload();
 });
 
 const cancelForm = document.querySelector(".cancelBtn");
@@ -35,99 +139,4 @@ cancelForm.addEventListener("click", () => {
   setTimeout(() => {
     fadeIn(createNewConfig, 220, "flex");
   }, 190);
-});
-
-const preConfiguredCardSection = document.querySelector(".preConfiguredSection");
-window.addEventListener("load", async () => {
-  const rawConfigData = await fetch("/config").then((res) => res.json());
-  const configData = rawConfigData.body;
-
-  if (configData.length == 0) {
-    return preConfiguredCardSection.insertAdjacentHTML("beforeend", emptyText);
-  }
-
-  configData.forEach((config) => {
-    let configType;
-    console.log(config.APIKey);
-    configData.type == "on" ? (configType = "Cloud") : (configType = "Local");
-    const configCard = `      <div class="preConfiguredCard" id="${config._id}">
-    <div class="preConfiguredHeaderBox">
-        <div class="preConfiguredTitle">${config.name}</div>
-        <div class="preConfiguredIconsSection">
-        <div class="preConfiguredIcon preConfigured-settings">
-          <span class="material-icons refresh-icon" id="preConfiguredIcon">refresh</span>
-        </div>
-        <div class="preConfiguredIcon preConfigured-edit">
-          <span class="material-icons edit-icon" id="preConfiguredIcon">edit</span>
-        </div>
-        <div class="preConfiguredIcon preConfigured-settings">
-          <span class="material-icons settings-icon" id="preConfiguredIcon">settings</span>
-        </div>
-        <div class="preConfiguredIcon preConfigured-delete">
-          <span class="material-icons delete-icon" id="preConfiguredIcon">delete</span>
-        </div>
-      </div>
-    </div>
-    <div class="preConfiguredBody">
-      <div class="split-section pcDetails">
-        <div class="detail-flex">
-          <div class="detailIcon">
-            <span class="material-icons cloudIcon">cloud</span>
-          </div>
-          <div class="detailField">Type</div>
-          <div class="detailText">${configType}</div>              
-        </div>
-        <div class="detail-flex">
-          <div class="detailIcon">
-            <span class="material-icons locationIcon">fmd_good</span>
-          </div>
-          <div class="detailField">Address</div>
-          <div class="detailText detailText-small">${config.target}</div>              
-        </div>
-        <div class="detail-flex">
-          <div class="detailIcon">
-            <span class="material-icons lockIcon">lock</span>
-          </div>
-          <div class="detailField">API Key</div>
-          <div class="detailText detailText-small">${config.APIKey}</div>              
-        </div>
-        <div class="detail-flex">
-          <div class="detailIcon">
-            <span class="material-icons tagIcon">tag</span>
-          </div>
-          <div class="detailField">App ID</div>
-          <div class="detailText">${config.appID}</div>              
-        </div>
-      </div>
-      <div class="split-section pc-right-split">
-        <div class="right-split-item deviceCountContainer">
-          <div class="deviceCount-item deviceCountNumber">COUNT</div>
-          <div class="deviceCount-item deviceCountText">Results</div>
-        </div>   
-      </div>          
-    </div>
-  </div>`;
-    preConfiguredCardSection.insertAdjacentHTML("beforeEnd", configCard);
-  });
-});
-
-preConfiguredCardSection.addEventListener("click", (e) => {
-  if (e.target.classList.contains("delete-icon")) {
-    console.log(e.target);
-    const targetConfig = e.target.closest(".preConfiguredCard");
-    const reqBody = { id: targetConfig.id };
-
-    fetch(`/config`, {
-      method: "delete",
-      body: JSON.stringify(reqBody),
-      headers: { "content-type": "application/json" },
-    })
-      .then((response) => {
-        console.log(response);
-        fadeOut(targetConfig, 180);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
 });
