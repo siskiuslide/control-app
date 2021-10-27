dc6a1b9a9c5f7210113e88bc006869227b97e245
siskiuslide <jakewhatling2002@gmail.com>
2021-10-27 12:02:22 +0100

    Code organisation

--- controllers/deviceControllers.js
+++ controllers/deviceControllers.js
@@ -4,26 +4,23 @@ const Device = require("../models/deviceModel");
 const Config = require("./../models/configModel");
 const fetch = require("node-fetch");
 const catchAsync = require("./helpers/catchAsync");
+const AppError = require('./../utils/error')
 
+//
+//----------------
+//
 exports.getDevices = catchAsync(async (req, res, next) => {
   //Get the associated config
   const assocConfig = await Config.find({ _id: req.params.id });
   //build the url for that config | prettier-ignore
-  const url = urlHelper.buildURL(
-    assocConfig[0].type,
-    assocConfig[0].target,
-    assocConfig[0].appID,
-    assocConfig[0].APIKey,
-    "all"
-  );
+  const url = urlHelper.buildURL(assocConfig[0].type, assocConfig[0].target, assocConfig[0].appID, assocConfig[0].APIKey, "all");
   //make request
   const hubResponse = await hubRequest(url);
   //Array to push db results to
   const resData = [];
-
   //Create a document for each device
   hubResponse.forEach(async (item, i) => {
-    //create a doc to upload to db
+    //create a doc to upload to db 
     const device = {
       configID: req.params.id,
       deviceID: item.id,
@@ -34,7 +31,6 @@ exports.getDevices = catchAsync(async (req, res, next) => {
       commands: item.commands,
       date: item.date,
     };
-
     //check whether the doc already exists, if not, add it
     Device.exists({ configID: device.configID, deviceID: device.deviceID}, async function (err, result) {
       if (result === true) {
@@ -55,7 +51,7 @@ exports.getDevices = catchAsync(async (req, res, next) => {
         await Device.create(device).catch((err) => console.log(err));
       }
       if (err) {
-        console.log(err);
+        new AppError(400, err.message);
       }
     });
   });
@@ -64,6 +60,10 @@ exports.getDevices = catchAsync(async (req, res, next) => {
   storedDevices.forEach((device) => resData.push(device));
   return res.status(200).json({ status: "Success", data: resData });
 });
+
+//
+//-------------
+//
 
 exports.updateDevice = catchAsync(async function (req, res, next) {
   //check which properties are being updated and update them.
@@ -78,6 +78,10 @@ exports.updateDevice = catchAsync(async function (req, res, next) {
   return res.status(200).json({ status: "Success", data: req.body });
 });
 
+//
+//--------------
+//
+
 exports.getSingleDevice = catchAsync(async function (req, res, next) {
   const device = await Device.findOneAndUpdate({
     configID: req.params.configID,
@@ -86,35 +90,24 @@ exports.getSingleDevice = catchAsync(async function (req, res, next) {
   return res.status(200).json({ status: "Success", data: device });
 });
 
+//
+//--------------------
+//
+
 exports.changeDeviceState = catchAsync(async function (req, res, next) {
   const assocDevice = await Device.find({ configID: req.params.configID, deviceID: req.params.deviceID })
     .then((data) => {
-      return data;
-    })
+      return data})
     .catch((err) => console.log(err));
 
   const assocConfig = await Config.find({ _id: req.params.configID }).catch((err) => console.log(err));
-  //build url to make request
-  const stateChangeUrl = urlHelper.buildURL(
-    assocConfig[0].type,
-    assocConfig[0].target,
-    assocConfig[0].appID,
-    assocConfig[0].APIKey,
-    req.params.deviceID,
-    `/${req.params.status}`
-  );
+  //build url to make request | prettier-ignore
+  const stateChangeUrl = urlHelper.buildURL(assocConfig[0].type, assocConfig[0].target, assocConfig[0].appID, assocConfig[0].APIKey, req.params.deviceID, `/${req.params.status}`);
   //make request - this does not return the new state
   const hubResponse = await hubRequest(stateChangeUrl);
 
-  //get new status from hub
-  const newStatusUrl = urlHelper.buildURL(
-    assocConfig[0].type,
-    assocConfig[0].target,
-    assocConfig[0].appID,
-    assocConfig[0].APIKey,
-    req.params.deviceID,
-    ""
-  );
+  //get new status from hub | prettier-ignore
+  const newStatusUrl = urlHelper.buildURL(assocConfig[0].type, assocConfig[0].target, assocConfig[0].appID, assocConfig[0].APIKey, req.params.deviceID, "");
   const newStatusFromHub = await hubRequest(newStatusUrl);
   //update db with new state
   const newStatus = newStatusFromHub.attributes.find((attr) => attr.name == "switch");
@@ -125,6 +118,10 @@ exports.changeDeviceState = catchAsync(async function (req, res, next) {
 
   return res.status(200).json({ status: "success", data: newStatusFromHub });
 });
+
+//
+//------------------
+//
 
 exports.deleteDevice = function (req, res, next) {
   res.send("x");
