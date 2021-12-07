6f5cd87ec40ecfae27573c9a7927c0a5c43ef42e
siskiuslide <jakewhatling2002@gmail.com>
2021-12-07 21:38:30 +0000

    Home page styling v2

--- public/home.js
+++ public/home.js
@@ -1,11 +1,18 @@
-const steps = document.querySelectorAll('.step')
-steps.forEach(step=>{
-    const stepIcon = step.querySelector('.material-icons')
-    console.log(stepIcon)
-    step.addEventListener('mouseenter',()=>{
-        stepIcon.style.transform = 'scale(1.1)'
-    })
-    step.addEventListener('mouseout',()=>{
-        stepIcon.style.transform = null
-    })
-})
+const steps = document.querySelectorAll(".step");
+steps.forEach((step) => {
+  const stepIcon = step.querySelector(".material-icons");
+  console.log(stepIcon);
+  step.addEventListener("mouseenter", () => {
+    stepIcon.style.transform = "scale(1.1)";
+  });
+  step.addEventListener("mouseout", () => {
+    stepIcon.style.transform = null;
+  });
+});
+
+const aboutContainer = document.querySelector(".aboutContainer");
+window.addEventListener("load", () => {
+  const stepFadeIn = new Promise(() => {
+    progressiveFadeIn(steps, 200, "flex");
+  }).then(fadeIn(aboutContainer, 75, "flex"));
+});
