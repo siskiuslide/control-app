70bcdca852526b7ed53cce452b50d3bd879a4c0b
siskiuslide <jakewhatling2002@gmail.com>
2021-12-09 07:13:02 +0000

    About page - scroll button behaviour & logic

--- public/about.js
+++ public/about.js
@@ -1,7 +1,6 @@
 const steps = document.querySelectorAll(".step");
 steps.forEach((step) => {
   const stepIcon = step.querySelector(".material-icons");
-  console.log(stepIcon);
   step.addEventListener("mouseenter", () => {
     stepIcon.style.transform = "scale(1.1)";
   });
@@ -12,8 +11,15 @@ steps.forEach((step) => {
 
 const mainHeading = document.querySelector(".openingImageHeading");
 const aboutContainer = document.querySelector(".aboutContainer");
-const featuresSection = document.querySelector('.featuresSection')
-const linksSection = document.querySelector('.linksSection')
+const aboutBanner = document.querySelector(".aboutBanner");
+const featuresSection = document.querySelector(".featuresSection");
+const linksSection = document.querySelector(".linksSection");
+const scrollBtnContainer = document.querySelector(".arrowContainer");
+const scrollDownBtns = document.querySelectorAll(".scrollDownBtn");
+const scroll1 = document.getElementById("scroll1");
+const scroll2 = document.getElementById("scroll2");
+
+//chain of instructions to load elements when page is opened
 window.addEventListener("load", () => {
   const stepFadeIn = new Promise(() => {
     fadeIn(mainHeading, 150, "block");
@@ -27,13 +33,33 @@ window.addEventListener("load", () => {
       setTimeout(() => {
         fadeIn(aboutContainer, 400, "flex");
       }, 600)
-    ).then(
+    )
+    .then(
       setTimeout(() => {
         fadeIn(featuresSection, 600, "flex");
       }, 600)
-    ).then(
+    )
+    .then(
       setTimeout(() => {
         fadeIn(linksSection, 600, "flex");
       }, 600)
     )
+    .then(
+      setTimeout(() => {
+        fadeIn(scrollBtnContainer, 500, "block");
+      }, 800)
+    );
+});
+
+const scrollDest = document.querySelectorAll(".scrollDest");
+
+scrollDownBtns.forEach((btn, i) => {
+  const id = btn.id.slice(6);
+  const target = document.getElementById(`dest${id}`).getBoundingClientRect();
+  console.log(target);
+  const destination = target.top - 40;
+  console.log(destination);
+  btn.addEventListener("click", () => {
+    window.scrollTo({ top: destination, left: 0, behavior: "smooth" });
+  });
 });
