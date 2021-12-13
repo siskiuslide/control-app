ba3dc1930bc96a57f0afe931342ab478ff68a6f0
siskiuslide <jakewhatling2002@gmail.com>
2021-12-13 17:00:32 +0000

    About page - SOLIS

--- public/about.js
+++ public/about.js
@@ -98,3 +98,47 @@ demoControls.forEach(control=>{
     }
   })
 })
+
+
+//SOLIS//
+const SOLIS = document.querySelector('.SOLIS')
+const SOLISAltText = document.querySelector('.altLink')
+const SOLISForm = document.querySelector('.solisItem')
+const SOLISHeading = SOLISForm.querySelector('h2')
+const SOLISConfirm = document.querySelectorAll('.solisConfirm')
+const SOLISButton = SOLISForm.querySelector('.button')
+let SOLISState
+SOLIS.classList.contains('signUp') ? SOLISState = 'signUp' : SOLISState = 'login';
+
+SOLISAltText.addEventListener('click',(e)=>{
+  e.preventDefault()
+  SOLISAltText.style.color = 'rgb(71,64,64)'
+  SOLISForm.classList.toggle('signUp')
+  SOLISForm.classList.toggle('login')
+  SOLISForm.classList.contains('signUp') ? SOLISState = 'signUp' : SOLISState = 'login';
+
+  if(SOLISState == 'signUp'){
+    SOLISHeading.textContent = 'Sign Up'
+    SOLISAltText.textContent = 'Already have an account?'
+    SOLISConfirm.forEach(el=>el.style.display = 'inline')
+    SOLISButton.textContent = 'Sign Up'
+    SOLISForm.style.transition = "transform 200ms"
+    SOLISForm.style.transform = "translateY(0px)"
+    SOLISButton.parentElement.style.transition = "transform 200ms"
+    SOLISButton.parentElement.style.transform = "translateY(0px)"
+    fadeOut(SOLISForm.querySelector('.forgotPassword'), 200)
+
+  }
+  if(SOLISState == 'login'){
+    SOLISHeading.textContent = 'Login'
+    SOLISAltText.textContent = 'Create an account'
+    SOLISConfirm.forEach(el=>el.style.display = 'none')
+    SOLISButton.textContent = 'Login'
+    SOLISForm.style.transition = "transform 200ms"
+    SOLISForm.style.transform = "translateY(30px)"
+    SOLISButton.parentElement.style.transition = "transform 300ms"
+    SOLISButton.parentElement.style.transform = "translateY(-30px)"
+    fadeIn(SOLISForm.querySelector('.forgotPassword'), 200, 'inline')
+  }
+
+})
