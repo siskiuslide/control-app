488b405b8a640ebf6889173a30eaa770b134737c
siskiuslide <jakewhatling2002@gmail.com>
2021-12-22 10:05:10 +0000

    Added documentation page & new feature on About.html

--- public/script.js
+++ public/script.js
@@ -3,63 +3,62 @@
 const emptyText = `<div class="error">
   <span class="material-icons">error</span>
   <p></p>
-</div>`
-const throwError = function(targetEl, position, message, additionalClass){
+</div>`;
+const throwError = function (targetEl, position, message, additionalClass) {
   document.querySelector(`${targetEl}`).insertAdjacentHTML(position, emptyText);
-  const error = document.querySelector('.error')
-  error.querySelector('p').textContent = message;
+  const error = document.querySelector(".error");
+  error.querySelector("p").textContent = message;
   error.classList.add(additionalClass);
-  fadeIn(error, 150, 'flex')
-
-}
+  fadeIn(error, 150, "flex");
+};
 
 //navbar elements
-const navBar = document.querySelector('.navbar')
-const navItems = document.querySelectorAll('.nav-item')
-const navItemText = document.querySelectorAll('.nav-item-text')
-const loginButton = document.querySelector('login-account')
-const activeItem = document.querySelector('.nav-active')
-const activeItemText = document.querySelector('.nav-text-active')
-let hoverStyle //Item hover style (dark)
-let originalTextColor
-let activeTextStyle = 'var(--midAccent)'
-let activeTextHoverStyle //style active text when hovered
+const navBar = document.querySelector(".navbar");
+const navItems = document.querySelectorAll(".nav-item");
+const navItemText = document.querySelectorAll(".nav-item-text");
+const loginButton = document.querySelector("login-account");
+const activeItem = document.querySelector(".nav-active");
+const activeItemText = document.querySelector(".nav-text-active");
+let hoverStyle; //Item hover style (dark)
+let originalTextColor;
+let activeTextStyle = "var(--midAccent)";
+let activeTextHoverStyle; //style active text when hovered
 
 //homepage navbar styling logic
- if(navBar.classList.contains('homeNav')){
-  originalTextColor = 'black'
-  hoverStyle = 'rgba(255, 255, 255, 0.466)';
-  activeTextHoverStyle = 'black'
-  navItems.forEach(item=>{
-    item.style.borderRight = null
-    const itemText = item.querySelector('.nav-item-text')
-    itemText.style.color = 'black'
-  })
-}else{
-  hoverStyle = 'rgb(60,60,60)'
-  activeTextHoverStyle = 'white'
-  originalTextColor = 'white'
+if (navBar.classList.contains("homeNav")) {
+  originalTextColor = "black";
+  hoverStyle = "rgba(255, 255, 255, 0.466)";
+  activeTextHoverStyle = "black";
+  navItems.forEach((item) => {
+    item.style.borderRight = null;
+    const itemText = item.querySelector(".nav-item-text");
+    itemText.style.color = "black";
+  });
+} else {
+  hoverStyle = "rgb(60,60,60)";
+  activeTextHoverStyle = "white";
+  originalTextColor = "white";
 }
 //apply styles set in logic above
-navItems.forEach(item=>{
-  const itemText  = item.querySelector('.nav-item-text')
-  activeItemText.style.color = activeTextStyle
-  item.addEventListener('mouseenter',()=>{
-    item.style.background = hoverStyle
+navItems.forEach((item) => {
+  const itemText = item.querySelector("a").querySelector("p");
+  activeItemText.style.color = activeTextStyle;
+  item.addEventListener("mouseenter", () => {
+    item.style.background = hoverStyle;
 
-    if(item.classList.contains('nav-active')){
-      itemText.style.color = activeTextHoverStyle
+    if (item.classList.contains("nav-active")) {
+      itemText.style.color = activeTextHoverStyle;
     }
-  })
-  item.addEventListener('mouseout',()=>{
-    item.style.background = null
-    if(item.classList.contains('nav-active')){
+  });
+  item.addEventListener("mouseout", () => {
+    item.style.background = null;
+    if (item.classList.contains("nav-active")) {
       itemText.style.color = activeTextStyle;
-    }else{
-      itemText.style.color = originalTextColor
+    } else {
+      itemText.style.color = originalTextColor;
     }
-  })
-})
+  });
+});
 
 //Generic UI Features
 let fadeOut = function (element, duration) {
@@ -79,15 +78,17 @@ let fadeIn = function (element, duration, orntn) {
 };
 
 //this is to fade in elements that are targets of el.GetBoundingClientRect
-let fadeInOpacOnly = function(element, duration){
-  element.style.transition = `opacity ${duration}ms`
-  element.style.opacity = '1'
-}
+let fadeInOpacOnly = function (element, duration) {
+  element.style.transition = `opacity ${duration}ms`;
+  element.style.opacity = "1";
+};
 
-let progressiveFadeIn = function (nodelist, interval, orntn) { // take a nodelist and fade in on interval
+let progressiveFadeIn = function (nodelist, interval, orntn) {
+  // take a nodelist and fade in on interval
   nodelist.forEach((el, i) => fadeIn(el, interval * i, orntn));
 };
-let progressiveFadeOut = function (nodelist, interval) { //take a nodelist and fade each one out using an interval
+let progressiveFadeOut = function (nodelist, interval) {
+  //take a nodelist and fade each one out using an interval
   nodelist.forEach((el, i) => {
     fadeOut(el, interval * i);
     el.remove();
@@ -99,11 +100,10 @@ const getDevices = async (config) => {
   const data = await fetch(`/config/${config}/devices`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data)
+      console.log(data);
       return data;
     })
-    .catch((err) => console.log(err)
-    );
+    .catch((err) => console.log(err));
   return data;
 };
 
@@ -121,38 +121,41 @@ const setFavIconStyle = (obj) => {
 //favourite item (change data or just icon)
 const favouriteItemIcon = function (e, typeClassName, buttonOnly, secondaryIcon) {
   //favourited class has yellow text
-  e.target.classList.toggle('.favourited')
+  e.target.classList.toggle(".favourited");
   let favouriteStatus;
   //hard to read but works
   if (e.target.classList.contains("favourited")) {
     e.target.textContent = "star";
-    if(buttonOnly == false){
+    if (buttonOnly == false) {
       favouriteStatus = true;
     }
   } else {
     e.target.textContent = secondaryIcon;
-    if(buttonOnly==false){
+    if (buttonOnly == false) {
       favouriteStatus = false;
     }
   }
-  if(buttonOnly == false){
+  if (buttonOnly == false) {
     const parent = e.target.closest(`.${typeClassName}`);
     const favouriteUpdate = {
       configID: parent.dataset.configid,
       favourite: favouriteStatus,
     };
     //for controls
-    if(typeClassName == 'control'){favouriteUpdate.deviceID = parent.id}
+    if (typeClassName == "control") {
+      favouriteUpdate.deviceID = parent.id;
+    }
     return favouriteUpdate;
   }
 };
 
-const getFavouriteItems = async function(type, favouriteQuery){
-  let favQueryResult
-  if(type == 'config'){
-    favQueryResult = await fetch(`/${type}?favourite=${favouriteQuery}`).then(res=>res.json()).catch(err=>console.log(err))
+const getFavouriteItems = async function (type, favouriteQuery) {
+  let favQueryResult;
+  if (type == "config") {
+    favQueryResult = await fetch(`/${type}?favourite=${favouriteQuery}`)
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
   }
-  if(type == 'devices'){
-    
+  if (type == "devices") {
   }
-}
+};
