a3eb7c1772b70a8ad3a612151f9602cee3811397
siskiuslide <jakewhatling2002@gmail.com>
2022-01-10 11:59:10 +0000

    Added a custom 404 page - not yet sent to client

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const AppError = require("./utils/error");
 const globalErrorHandler = require("./controllers/errorController");
+const authController = require('./controllers/authController')
 const app = express();
 
 app.use(express.json());
@@ -24,9 +25,13 @@ app.use("/devices", deviceRouter);
 
 app.use("/users", userRouter);
 
+
 app.all("*", (req, res, next) => {
+  console.log(__dirname)
+  res.status(404).render(path.join(__dirname, 'public/404.html'))
   next(new AppError(`Can't find ${req.originalUrl} on the server`, 400));
 });
 app.use(globalErrorHandler);
+
 
 module.exports = app;
