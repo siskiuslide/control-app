80fb8c1faf5805e2ed756afe0d2e6ba65e798c3a
siskiuslide <jakewhatling2002@gmail.com>
2022-01-10 06:10:58 +0000

    Users - added /updateMe route for logged in users who wish to update their details

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,16 @@ const catchAsync = require("./helpers/catchAsync");
 const APITools = require("../utils/APITools");
 const AppError = require("./../utils/error");
 
+const filterObject = function (object, ...allowedFields) {
+  const filtered = {};
+  Object.keys(object).forEach((key) => {
+    if (allowedFields.includes(key)) {
+      filtered[key] = object[key];
+    }
+  });
+  return filtered;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
   res.status(200).json({ status: "Success", data: users });
@@ -24,4 +34,25 @@ exports.updateUser = catchAsync(async (req, res, next) => {
   const updateObject = req.body;
   const user = await User.findOneAndUpdate({ _id: req.params.id }, { updateObject });
   res.status(200).json({ status: "Success", data: user });
+});
+
+//for currently authenticated user
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //if body contains password create an error
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError("Cannot update password from this route", 400));
+  }
+
+  //filter fields that cannot be updated
+  const updateObject = filterObject(req.body, "name", "email");
+
+  //update document
+  const user = await User.findByIdAndUpdate(req.user.id, updateObject, { new: true, runValidators: true }); //we can use update rather than save here because we don't need the password validator to run
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
 });
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,10 +6,13 @@ const router = express.Router();
 router.route("/signup").post(authController.signup);
 router.route("/login").post(authController.login);
 
+//for users
 router.route("/forgotPassword").post(authController.forgotPassword);
 router.route("/resetPassword/:token").patch(authController.resetPassword);
 router.patch("/updateMyPassword", authController.protectRoute, authController.updatePassword);
+router.patch("/updateMe", authController.protectRoute, userController.updateMe);
 
+//accessible by admins/devs
 router
   .route("/")
   .get(authController.protectRoute, authController.restrictTo("dev"), userController.getAllUsers)
