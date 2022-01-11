dcad1ccb9c2f197d6d943c1ee1182bf6ef86d8ae
siskiuslide <jakewhatling2002@gmail.com>
2022-01-11 06:27:47 +0000

    Added helmet package for security headers

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const AppError = require("./utils/error");
 const globalErrorHandler = require("./controllers/errorController");
 const authController = require("./controllers/authController");
 const app = express();
 
+app.use(helmet()); //security headers are the first middleware
+
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+  app.use(morgan("dev")); //logging
 }
 
-//upto 100 requests per hour
 const limiter = rateLimit({
   max: 200,
   windowMs: 60 * 60 * 1000,
@@ -18,10 +20,9 @@ const limiter = rateLimit({
 });
 
 app.use(limiter); //if server is restarted the limiter is reset
-
-app.use(express.json());
-app.use(express.static("./public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10kb" })); //allows json requests 7 limit size
+app.use(express.static("./public")); // serves html files from /public
+app.use(express.urlencoded({ extended: true })); //allows incoming strings/arrs
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -32,6 +33,7 @@ const deviceRouter = require("./routes/deviceRouter");
 const configRouter = require("./routes/configRouter");
 const userRouter = require("./routes/userRouter");
 
+//API
 app.use("/config", configRouter);
 
 app.use("/devices", deviceRouter);
