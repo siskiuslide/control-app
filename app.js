345e4638fe344412d4406f4bab75ef5806f86795
siskiuslide <jakewhatling2002@gmail.com>
2022-01-11 06:18:56 +0000

    Added express rate limiting to global middlewares

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const morgan = require("morgan");
 const AppError = require("./utils/error");
 const globalErrorHandler = require("./controllers/errorController");
-const authController = require('./controllers/authController')
+const authController = require("./controllers/authController");
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//upto 100 requests per hour
+const limiter = rateLimit({
+  max: 200,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP. Please try again later",
+});
+
+app.use(limiter); //if server is restarted the limiter is reset
 
 app.use(express.json());
 app.use(express.static("./public"));
-app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
@@ -25,11 +38,9 @@ app.use("/devices", deviceRouter);
 
 app.use("/users", userRouter);
 
-
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
 });
 app.use(globalErrorHandler);
-
 
 module.exports = app;
